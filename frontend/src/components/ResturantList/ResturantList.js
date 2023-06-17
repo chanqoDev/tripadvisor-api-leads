429189cfd248616bea83e7ee4d4b71aa3458183a
Christian E <christianelena92@gmail.com>
2023-06-16 23:44:46 -0500

    Refactor: Refactor SearchRestaurant component
    The commit refactors the SearchRestaurant component by moving the code from the App.js file to its own dedicated component. The component now fetches restaurant data dynamically using the fetchRestaurantData function from the ../../services/api module.
    
    Changes:
    - Moved SearchRestaurant code from App.js to its own component
    - Implemented dynamic fetching of restaurant data
    - Updated table rendering with dynamic data
    - Added expand/collapse functionality to display additional restaurant information
    - Updated styling and component dependencies

--- frontend/src/components/ResturantList/ResturantList.js
+++ frontend/src/components/ResturantList/ResturantList.js
@@ -1,7 +1,143 @@
-// import React, { useEffect, useState } from "react";
-// import { getResturantData } from "../../services/api";
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { fetchRestaurantData } from "../../services/api";
 
-// function RestaurantList() {
-//   const [data, setRestaurantData] = useState([]);
+function SearchRestaurant() {
+  const [restaurants, setRestaurantData] = useState([]);
+  useEffect(() => {
+    const locationIds = ["38954"]; //   LS : 44588, KC : 38815, Ola : 38954 Lenexa: 3497938
+    const fetchData = async () => {
+      try {
+        const allData = await Promise.all(
+          locationIds.map(async (locationId) => {
+            const data = await fetchRestaurantData(locationId);
+            return data;
+          })
+        );
+        const mergedData = allData.flat();
+        setRestaurantData(mergedData);
+        console.log(mergedData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-// }
+  const [openRows, setOpenRows] = useState([]);
+  const handleRowToggle = (restaurantsId) => {
+    if (openRows.includes(restaurantsId)) {
+      setOpenRows(openRows.filter((rowId) => rowId !== restaurantsId));
+    } else {
+      setOpenRows([...openRows, restaurantsId]);
+    }
+  };
+
+  return (
+    // Dynamic Loading Resturant Search
+    <div className="App">
+      {restaurants.length > 0 ? (
+        <div>
+          <h2>Restaurant Data</h2>
+          <TableContainer
+            component={Paper}
+            sx={{ maxWidth: 900, margin: "auto" }}
+            style={{ marginBottom: 10, marginTop: 20 }}
+          >
+            <Table aria-label="collapsible table">
+              <TableHead style={{ border: "unset" }}>
+                <TableRow>
+                  <TableCell />
+                  <TableCell align="center" style={{ fontWeight: "bold" }}>
+                    Restaurant Name
+                  </TableCell>
+                  <TableCell align="right" style={{ fontWeight: "bold" }}>
+                    Status
+                  </TableCell>
+                  <TableCell align="center" style={{ fontWeight: "bold" }}>
+                    City
+                  </TableCell>
+                  <TableCell align="center" style={{ fontWeight: "bold" }}>
+                    RestaurantsId
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {restaurants.map((data) => (
+                  <React.Fragment key={data.restaurantsId}>
+                    <TableRow>
+                      <TableCell>
+                        <IconButton
+                          aria-label="expand row"
+                          size="small"
+                          onClick={() => handleRowToggle(data.restaurantsId)}
+                        >
+                          {openRows.includes(data.restaurantsId) ? (
+                            <KeyboardArrowUpIcon />
+                          ) : (
+                            <KeyboardArrowDownIcon />
+                          )}
+                        </IconButton>
+                      </TableCell>
+                      <TableCell align="center" component="th" scope="row">
+                        {data.name}
+                      </TableCell>
+                      <TableCell align="right">
+                        {data.currentOpenStatusText}
+                      </TableCell>
+                      <TableCell align="right">{data.parentGeoName}</TableCell>
+                      <TableCell align="center">{data.restaurantsId}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell
+                        style={{ paddingBottom: 0, paddingTop: 0 }}
+                        colSpan={6}
+                      >
+                        <Collapse
+                          in={openRows.includes(data.restaurantsId)}
+                          timeout="auto"
+                          unmountOnExit
+                        >
+                          <Box sx={{ margin: 1 }}>
+                            <Typography
+                              variant="h6"
+                              gutterBottom
+                              component="div"
+                            >
+                              Additional Information
+                            </Typography>
+                            <img
+                              src={data.heroImgUrl}
+                              alt={data.name}
+                              height="auto"
+                              width="250"
+                            />
+                          </Box>
+                        </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  </React.Fragment>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      ) : (
+        <p>No restaurant data available.</p>
+      )}
+    </div>
+  );
+}
+export default SearchRestaurant;
